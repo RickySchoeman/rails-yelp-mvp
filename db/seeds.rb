e2a3d37dcb0eff930a35a1235d1ad097bdd0b9da
ricky schoeman <schoeman.ricky@gmail.com>
2020-11-25 00:32:45 +0100

    incomplete challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+10.times do 
+  name = Faker::Restaurant.name
+  address = Faker::Address.city
+  phone_number = Faker::PhoneNumber.phone_number
+  category = CATEGORY.sample
+restaurant = Restaurant.create(name: name, address: address, phone_number: phone_number, category: category)
+puts "Created #{restaurant.name}"
+end 
+
+puts "Finished!"
